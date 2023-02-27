a63e67bed07ebd0d71bc8987b59779b6ee41c851
k_miyamoto <k_miyamoto@ga-tech.co.jp>
2023-02-27 15:46:57 +0900

    add user account settings

---
+++ pages/account.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    account.vue
+    <nuxt-child />
+  </v-container>
+</template>
+
+<script>
+  export default {
+    layout: 'loggedIn',
+    // falseを返すページのアクセスを制限する => OK:/account/passowrd     NG:/account
+    validate ({ route}) {
+      return route.name !== 'account'
+    }
+  }
+</script>
